9ed9732f862303ad247b64b0f18aa6910a9c6f16
Suriya Chellappan <71540689+suriya911@users.noreply.github.com>
2024-11-17 22:49:05 -0800

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,37 @@
+import type { Metadata } from "next";
+import { Inter, Calistoga } from "next/font/google";
+import "./globals.css";
+import { twMerge } from "tailwind-merge";
+
+const intern = Inter({ subsets: ["latin"], variable: "--font-sans" });
+
+const calistoga = Calistoga({
+  subsets: ["latin"],
+  variable: "--font-serif",
+  weight: ["400"],
+});
+
+export const metadata: Metadata = {
+  title: "Suriya's Portfolio",
+  description: "Created to Show My profile to the World",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={twMerge(
+          intern.variable,
+          calistoga.variable,
+          "bg-gray-900 text-white antialiased font-sans"
+        )}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
